bd13901da719bca079bd0ab43f7e8427e5e090b9
xiaoguoyu <xiao_guoyu@126.com>
2021-07-29 14:58:52 +0800

    websockets成功！

--- websocket_02_server.py
+++ websocket_02_server.py
@@ -1,2 +1,13 @@
 import asyncio
-import 
+import websockets
+
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    if name == "hello server!":
+        await websocket.send("hello client!")
+
+start_server = websockets.serve(hello, "0.0.0.0", 39444)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()

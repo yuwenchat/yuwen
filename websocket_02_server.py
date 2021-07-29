768d40dac106b3449ef1e28681e15a5b37bd7e7d
xiaoguoyu <xiao_guoyu@126.com>
2021-07-29 21:22:35 +0800

    注册登录雏形

--- websocket_02_server.py
+++ websocket_02_server.py
@@ -6,6 +6,12 @@ async def hello(websocket, path):
     name = await websocket.recv()
     if name == "hello server!":
         await websocket.send("hello client!")
+    else:
+        content = name.split("\n")
+        if content[0] == "login":
+            user = content[1]
+            pwd = content[2]
+            await  websocket.send(f"user's name is:{user}\npassword is:{pwd}")
 
 start_server = websockets.serve(hello, "0.0.0.0", 39444)
 
